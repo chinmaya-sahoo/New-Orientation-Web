580a645dc8a489ed4815bdf68e2fbe70c0030a8f
chinmay sahoo <chinmaysahoo668@gmail.com>
2025-02-05 19:40:09 +0530

    repo connect

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <h1 className='container bg-red-500 text-center text-4xl text-white'>
+        app
+      </h1>
+    </div>
+  )
+}
+
+export default App
